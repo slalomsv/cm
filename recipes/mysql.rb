25badc15d7586563607605101528446ce0f0bbf6
Kirk Larsen <kirkl@slalom.com>
2017-04-21 10:41:44 -0700

    break sql packaging for cm-centos-73, cm-oracle-68, cm-oracle-73

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -1,34 +1,9 @@
-platform = node['platform']
-version = node['platform_version']
-
-name = if version =~ /^7/ && (platform == 'centos')
-         'mariadb-server'
-       elsif version =~ /^7/ && (platform == 'oracle')
-         'mysql-community-common'
-       else
-         'mysql-server'
-       end
-
-execute 'yum_update' do
-  command 'yum update -y'
-  only_if { version =~ /^7/ && (platform == 'oracle') }
-end
-
-package name do
+package  'mysql-server' do
   action :install
 end
-if version =~ /^7/ && (platform == 'centos')
-  service 'mysql' do
-    service_name node['mariadb']['mysqld']['service_name']
-    supports restart: true
-    action :start
-  end
-elsif version =~ /^7/ && (platform == 'oracle')
-  
-else
-  mysql_service 'simple' do
-    port '3306'
-    initial_root_password 'change_me'
-    action [:create, :start]
-  end
+
+mysql_service 'simple' do
+  port '3306'
+  initial_root_password 'change_me'
+  action [:create, :start]
 end
